829eb929c716a116cdc52b684b38b8bda8d1fa46
koenraad.pecceu <koenraad.pecceu@gmail.com>
2025-02-14 09:27:03 +0100

    Update Adonaï

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,14 +1,36 @@
 "use strict";
 
 // formulier zelf koppelen
-const form = document.getElementById("form");
+const form = document.getElementById("form"); // <form>
 // alle input tags verzamelen
-const inputs = document.querySelectorAll("input");
+const inputs = document.querySelectorAll("input"); // alle <input>
 // errors div koppelen waarin we de foutboodschappen willen plaatsen
-const errorsDiv = document.getElementById("errors");
+const errorsDiv = document.getElementById("errors"); // <div id="errors">
 
 // code die uitgevoerd wordt
 // wanneer het formulier verstuurd wordt
-form.addEventListener("submit",function(){
+// standaard wordt de volledige pagina herladen
+// wanneer het formulier verstuurd wordt
+// omdat er geen specifieke action is
+form.addEventListener("submit",function(e){
     
+    // het herladen van de pagina standaard blokkeren
+    // voorkom de standaard action --> prevent default (action)
+    e.preventDefault();
+
+    // eerst styling weer verwijderen
+    // dus alle rode en groene randen weg
+    RemoveClasses();
+
 })
+
+// function die bij alle input tags
+// alle classes verwijdert
+// door gewoon het attribute class te verwijderen
+function RemoveClasses(){
+    inputs.forEach(element => {
+        // volledige class attribute verwijderen
+        // dus dan zijn alle classes weg
+        element.removeAttribute("class");
+    });
+}
